68ff9b2cb007f68ce06e4008ac51ee2913e6a960
Millimeter-Wave <alaekwebartholomew@gmail.com>
2023-01-25 20:58:34 -0800

    stack setup

--- stack/stack.js
+++ stack/stack.js
@@ -2,5 +2,22 @@ class Stack {
 
     constructor(){
         this.dataBase = []
+        this.count = 0
     }
+
+    push(data){}
+
+    pop(){}
+
+    remove(index){}
+
+    length(){}
+
+    isEmpty(){}
+
+    isPresent(data){}
+
+    print(){}
 }
+
+st = new Stack()
