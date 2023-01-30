fc8f7f0078e484343b03d7a4641b2a4e412387cc
Millimeter-Wave <alaekwebartholomew@gmail.com>
2023-01-30 06:54:50 -0800

    Singly linked list setup

---
+++ singlyLinkedList/singlylinkedlist.js
@@ -0,0 +1,29 @@
+class Node{
+    constructor(data, next) {
+        this.data = data
+        this.next = next
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.head = null
+    }
+
+    length(){}
+
+    print(){}
+
+    addAtBeginning(data){}
+
+    addAtEnd(data){}
+
+    addAtIndex(data, index){}
+
+    removeFirst(){}
+
+    removeLast(){}
+
+    removeAtIndex(index){}
+    
+}
